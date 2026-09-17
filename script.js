e4f9435882190247ba2f6ff9fc6f4fd51e3a94de
MADHAV ANAND <m47903237@gmail.com>
2026-09-17 18:19:25 +0530

    feat: implement updateflag function and working on fetch API

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const BASE_URL = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json';
-
+const BASE_URL = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/';
+const fromCurr = document.querySelector('.from select')
+const toCurr = document.querySelector('.to select')
 const dropdowns = document.querySelectorAll('.dropdown select');
-
+const btn = document.querySelector('.submit');
  for(let select of dropdowns) {
      for (let code in countryList) {
   let newOption = document.createElement('option');
@@ -20,6 +21,24 @@ const dropdowns = document.querySelectorAll('.dropdown select');
      updateflag(e.target)
  })
  }
- const updateflag = () => {
-
+ const updateflag = (element) => {
+let currCode  = element.value;
+let countryCode = countryList[currCode];
+let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+ let img = element.parentElement.querySelector('img');
+img.src = newSrc;
  }
+ btn.addEventListener('click', async (e) => {
+     e.preventDefault();
+     let amount = document.querySelector('.amount input');
+     let amountValue = amount.value;
+     if(amountValue === '' || amountValue === '' || amountValue < 1){
+         amountValue = 1;
+         amount.value = amountValue;
+     }
+
+// console.log(fromCurr.value , toCurr.value)
+const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+ let response = await fetch(URL);
+ console.log(response);
+ })
